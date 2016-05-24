a57518890d1e87432df3eaf9682cba2100baf65f
ThuYein <thuyeintun@my.jcu.edu.au>
2016-05-24 13:33:54 +0800

    Created area.py

---
+++ area.py
@@ -0,0 +1,10 @@
+print('Area & Volume calculator')
+
+width = int(input("Enter width : "))
+height = int(input("Enter height : "))
+depth = int(input("Enter depth : "))
+
+area = width * height
+volume = area * depth
+print("Area : ", area)
+print("Volume : ",volume)
